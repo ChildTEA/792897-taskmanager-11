edc6035b7f747bbd3f16d8581a1c9b598da1d53b
Yaroslav Smirnov <yar14@mail.ru>
2020-04-01 12:58:51 +0300

    feat (components rendering): Реализованы скрипты для отрисовки меню

---
+++ src/main.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const siteMainElement = document.querySelector(`.main`);
+const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
+
+const renderComponent = (container, template, place = `beforeend`) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+
+const createSiteMenuTemplate = () => {
+  return (
+    `<section class="control__btn-wrap">
+      <input
+        type="radio"
+        name="control"
+        id="control__new-task"
+        class="control__input visually-hidden"
+      />
+      <label for="control__new-task" class="control__label control__label--new-task"
+        >+ ADD NEW TASK</label
+      >
+      <input
+        type="radio"
+        name="control"
+        id="control__task"
+        class="control__input visually-hidden"
+        checked
+      />
+      <label for="control__task" class="control__label">TASKS</label>
+      <input
+        type="radio"
+        name="control"
+        id="control__statistic"
+        class="control__input visually-hidden"
+      />
+      <label for="control__statistic" class="control__label"
+        >STATISTICS</label
+      >
+    </section>`
+  );
+};
+
+renderComponent(siteHeaderElement, createSiteMenuTemplate());
