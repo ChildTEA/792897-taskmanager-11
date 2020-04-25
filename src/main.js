470f1f782dd9cc3f7b8d1f3e0036eeb001627fb7
Yaroslav Smirnov <yar14@mail.ru>
2020-04-25 19:34:27 +0300

    feat: Реализация первой части домашнего задания по лекции №4

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import LoadMoreButtonComponent from './components/load-more-button.js';
 import TaskEditComponent from './components/task-editor.js';
 import TaskComponent from './components/task.js';
 import TasksComponent from './components/tasks.js';
-import NoTasksComponent from './components/no-tasks.js';
 import SiteMenuComponent from './components/site-menu.js';
 import SortComponent from './components/sorting.js';
 import {generateTasks} from './mock/task.js';
@@ -57,13 +56,6 @@ const renderTask = (taskListElement, task) => {
 
 
 const renderBoard = (boardComponent, tasks) => {
-  const isAllTasksArchived = tasks.every((task) => task.isArchive);
-
-  if (isAllTasksArchived || tasks.length === 0) {
-    renderComponent(boardComponent.getElement(), new NoTasksComponent().getElement());
-    return;
-  }
-
   renderComponent(boardComponent.getElement(), new SortComponent().getElement());
   renderComponent(boardComponent.getElement(), new TasksComponent().getElement());
 
