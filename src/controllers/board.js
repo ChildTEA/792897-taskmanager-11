c0e24fdbb6a83bd25b73d74bf1f6531136af13db
Yaroslav Smirnov <yar14@mail.ru>
2020-05-22 21:42:15 +0300

    Feat: Реализовано переключение экранов.

--- src/controllers/board.js
+++ src/controllers/board.js
@@ -73,6 +73,14 @@ export default class BoardController {
     this._renderLoadMoreButton();
   }
 
+  show() {
+    this._container.show();
+  }
+
+  hide() {
+    this._container.hide();
+  }
+
   _removeTasks() {
     this._showedTaskControllers.forEach((taskController) => taskController.destroy());
     this._showedTaskControllers = [];
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import BoardComponent from './components/board.js';
 import BoardController from './controllers/board.js';
 import FilterController from "./controllers/filter.js";
 import SiteMenuComponent, {MenuItem} from './components/site-menu.js';
+import StatisticsComponent from './components/statistics.js';
 import TasksModel from './models/tasks.js';
 import {generateTasks} from './mock/task.js';
 import {render} from './utils/render-component.js';
@@ -22,16 +23,30 @@ filterController.render();
 const boardComponent = new BoardComponent();
 const boardController = new BoardController(boardComponent, tasksModel);
 const siteMenuComponent = new SiteMenuComponent();
+const statisticsComponent = new StatisticsComponent();
 
 render(siteHeaderElement, siteMenuComponent);
 render(siteMainElement, boardComponent);
+render(siteMainElement, statisticsComponent);
+
+statisticsComponent.hide();
 boardController.render(tasks);
 
 siteMenuComponent.setOnChange((menuItem) => {
   switch (menuItem) {
     case MenuItem.NEW_TASK:
       siteMenuComponent.setActiveItem(MenuItem.TASKS);
+      statisticsComponent.hide();
+      boardController.show();
       boardController.createTask();
+      break;
+    case MenuItem.STATISTICS:
+      boardController.hide();
+      statisticsComponent.show();
+      break;
+    case MenuItem.TASKS:
+      statisticsComponent.hide();
+      boardController.show();
       break;
   }
 });
